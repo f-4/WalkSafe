9a211a2c0c0aab2517f5f75067d144dc879db217
Brian Kim <briankim4311@gmail.com>
2017-09-06 02:01:47 -0700

    Dynamically render crimes nearby upon onRegionDidChange

--- app/components/base-map.js
+++ app/components/base-map.js
@@ -32,55 +32,42 @@ export default class BaseMap extends Component {
       latitude: 40.72052634,
       longitude: -73.97686958312988
     },
-    zoom: 11,
+    zoom: 13,
     userTrackingMode: Mapbox.userTrackingMode.none,
-    annotations: [{
-      coordinates: [40.72052634, -73.97686958312988],
-      type: 'point',
-      title: 'This is marker 1',
-      subtitle: 'It has a rightCalloutAccessory too',
-      rightCalloutAccessory: {
-        source: { uri: 'https://cldup.com/9Lp0EaBw5s.png' },
-        height: 25,
-        width: 25
-      },
-      annotationImage: {
-        source: { uri: 'https://cldup.com/CnRLZem9k9.png' },
-        height: 25,
-        width: 25
-      },
-      id: 'marker1'
-    }, {
-      coordinates: [40.714541341726175,-74.00579452514648],
-      type: 'point',
-      title: 'Important!',
-      subtitle: 'Neat, this is a custom annotation image',
-      annotationImage: {
-        source: { uri: 'https://cldup.com/7NLZklp8zS.png' },
-        height: 25,
-        width: 25
-      },
-      id: 'marker2'
-    }, {
-      coordinates: [[40.76572150042782,-73.99429321289062],[40.743485405490695, -74.00218963623047],[40.728266950429735,-74.00218963623047],[40.728266950429735,-73.99154663085938],[40.73633186448861,-73.98983001708984],[40.74465591168391,-73.98914337158203],[40.749337730454826,-73.9870834350586]],
-      type: 'polyline',
-      strokeColor: '#00FB00',
-      strokeWidth: 4,
-      strokeAlpha: .5,
-      id: 'foobar'
-    }, {
-      coordinates: [[40.749857912194386, -73.96820068359375], [40.741924698522055,-73.9735221862793], [40.735681504432264,-73.97523880004883], [40.7315190495212,-73.97438049316406], [40.729177554196376,-73.97180557250975], [40.72345355209305,-73.97438049316406], [40.719290332250544,-73.97455215454102], [40.71369559554873,-73.97729873657227], [40.71200407096382,-73.97850036621094], [40.71031250340588,-73.98691177368163], [40.71031250340588,-73.99154663085938]],
-      type: 'polygon',
-      fillAlpha: 1,
-      strokeColor: '#ffffff',
-      fillColor: '#0000ff',
-      id: 'zap'
-    }]
+    annotations: []
   };
 
   onRegionDidChange = (location) => {
     this.setState({ currentZoom: location.zoomLevel });
     console.log('onRegionDidChange', location);
+
+    var self = this;
+    axios.get('http://api.spotcrime.com/crimes.json', {params: {
+        lat: location.latitude,
+        lon: location.longitude,
+        key: "privatekeyforspotcrimepublicusers-commercialuse-877.410.1607",
+        radius: 0.01
+    }})
+      .then(function(response) {
+        var newCrimes = response.data.crimes.map(function(crime) {
+          return {
+            coordinates: [crime.lat, crime.lon],
+            type: 'point',
+            title: crime.type,
+            subtitle: `${crime.address} ${crime.date}`,
+            annotationImage: {
+              source: { uri: 'http://www.rw-designer.com/icon-image/6648-256x256x32.png' },
+              height: 25,
+              width: 25
+            },
+            id: crime.cdid.toString()
+          };
+        });
+
+        self.setState({
+          annotations: [...self.state.annotations, ...newCrimes]
+        });
+      });
   };
   onRegionWillChange = (location) => {
     console.log('onRegionWillChange', location);
