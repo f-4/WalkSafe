215b404e5407ec36d59d2f1e6a1d3f3d3521d4b9
fredesse <eleegia@gmail.com>
2017-09-07 15:21:16 -0700

    Move icons to their own folder

---
+++ app/components/icons/NoView.js
@@ -0,0 +1,6 @@
+import React, { Component } from 'react';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+const noViewIcon = (<Icon name="eye-slash" size={30} color="#000" />);
+
+export default noViewIcon;
