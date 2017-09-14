01fed6534ff340f7b8a476050b89fc1183b2406f
Rick Gallegos <duulketariakan@yahoo.com>
2017-09-14 16:07:19 -0700

    Clean package.json and Landing.js

--- app/screens/Landing.js
+++ app/screens/Landing.js
@@ -19,6 +19,7 @@
 //
 // export default Landing;
 
+
 import React, { Component } from 'react';
 import {
   Image,
@@ -30,77 +31,63 @@ import {
 } from 'react-native';
 import { FACEBOOK_URL, GOOGLE_URL } from 'react-native-dotenv';
 import Icon from 'react-native-vector-icons/FontAwesome';
-// import SafariView from 'react-native-safari-view';
+
 
 class Landing extends Component {
-  state = {
-    user: undefined, // user has not logged in yet
-  };
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      user: undefined, // user has not logged in yet
+    };
+
+    this.handleOpenURL = this.handleOpenURL.bind(this);
+    this.loginWithGoogle = this.loginWithGoogle.bind(this);
+    this.loginWithFacebook = this.loginWithGoogle.bind(this);
+    this.openURLhandler = this.openURLhandler.bind(this);
+  }
 
   componentDidMount() {
-    // Add event listener to OAuthLogin:// URLs
-    Linking.addEventListener('url', this.testMe);
+    Linking.addEventListener('url', this.handleOpenURL);
     // Launched from an external URL
-    console.log('Component did mount', this);
-
     Linking.getInitialURL().then((url) => {
-      console.log('What is the initial URL', url);
       if (url) {
-        this.testMe(url);
+        this.handleOpenURL({ url });
       }
     });
+
   };
+
 
   componentWillUnmount() {
     // Remove event listener
-    console.log('Does this component ever unmount');
     Linking.removeEventListener('url', this.handleOpenURL);
-  };
+  }
 
-  testMe = (url) => {
-    console.log(' I am the test listener', url);
-    const [, user_string] = url.url.match(/user=([^#]+)/);
-    this.setState({
-      // Decode the user string and parse it into JSON
-      user: JSON.parse(decodeURI(user_string))
-    });
-  };
-
-  handleOpenURL = ({ url }) => {
-    // Extract stringified user string out of the URL
+  handleOpenURL({ url }) {
+    // extract stringified user string out of the URL
     const [, user_string] = url.match(/user=([^#]+)/);
+
     this.setState({
-      // Decode the user string and parse it into JSON
+      // decode the user string and parse it into JSON
       user: JSON.parse(decodeURI(user_string))
     });
-    // if ( Platform.OS === 'ios') {
-    //   SafariView.dismiss();
-    // }
-  };
+  }
 
   // Handle Login with Facebook button tap
-  loginWithFacebook = () => {
+  loginWithFacebook() {
     console.log('here is the FB URL:', FACEBOOK_URL);
-    this.openURL(FACEBOOK_URL);
+    this.openURLhandler(FACEBOOK_URL);
   }
 
   // Handle Login with Google button tap
-  loginWithGoogle = () => {
+  loginWithGoogle() {
     console.log('here is the Google URL:', GOOGLE_URL);
-    this.openURL(GOOGLE_URL);
+    this.openURLhandler(GOOGLE_URL);
   }
-  openURL = (url) => {
-    // if (Platform.OS === 'ios') {
-    //   SafariView.show({
-    //     url: url,
-    //     fromBottom: true,
-    //   });
-    // }
-    // Or Linking.openURL on Android
-    // else {
-      Linking.openURL(url);
-    // }
-  };
+
+  openURLhandler(url) {
+    Linking.openURL(url);
+  }
 
   render() {
     const { user } = this.state;
@@ -195,172 +182,4 @@ const styles = StyleSheet.create({
   },
 });
 
-<<<<<<< 0519ff8b6be8adb3b93e569b9fb3f59344755c63
-// import React, { Component } from 'react';
-// import {
-//   Image,
-//   Linking,
-//   StyleSheet,
-//   Platform,
-//   Text,
-//   View
-// } from 'react-native';
-// import { FACEBOOK_URL, GOOGLE_URL } from 'react-native-dotenv';
-// import Icon from 'react-native-vector-icons/FontAwesome';
-
-
-// class Landing extends Component {
-//   constructor(props, context) {
-//     super(props, context);
-//     this.state = {
-//       user: undefined, // user has not logged in yet
-//     };
-
-//     this.handleOpenURL = this.handleOpenURL.bind(this);
-//     this.loginWithGoogle = this.loginWithGoogle.bind(this);
-//     this.loginWithFacebook = this.loginWithGoogle.bind(this);
-//     this.openURLhandler = this.openURLhandler.bind(this);
-//   }
-
-//   componentDidMount() {
-//     Linking.addEventListener('url', this.handleOpenURL);
-//     // Launched from an external URL
-//     Linking.getInitialURL().then((url) => {
-//       if (url) {
-//         this.handleOpenURL({ url });
-//       }
-//     });
-
-//   };
-
-
-//   componentWillUnmount() {
-//     // Remove event listener
-//     Linking.removeEventListener('url', this.handleOpenURL);
-//   }
-
-//   handleOpenURL({ url }) {
-//     // extract stringified user string out of the URL
-//     const [, user_string] = url.match(/user=([^#]+)/);
-
-//     this.setState({
-//       // decode the user string and parse it into JSON
-//       user: JSON.parse(decodeURI(user_string))
-//     });
-//   }
-
-//   // Handle Login with Facebook button tap
-//   loginWithFacebook() {
-//     console.log('this')
-//     console.log('here is the FB URL:', FACEBOOK_URL);
-//     this.openURLhandler(FACEBOOK_URL);
-//   }
-
-//   // Handle Login with Google button tap
-//   loginWithGoogle() {
-//     console.log('here is the Google URL:', GOOGLE_URL);
-//     this.openURLhandler(GOOGLE_URL);
-//   }
-
-//   openURLhandler(url) {
-//     Linking.openURL(url);
-//   }
-
-//   render() {
-//     const { user } = this.state;
-//     return (
-//       <View style={styles.container}>
-//         { user
-//           ? // Show user info if already logged in
-//             <View style={styles.content}>
-//               <Text style={styles.header}>
-//                 Welcome {user.name}!
-//               </Text>
-//               <View style={styles.avatar}>
-//                 <Image source={{ uri: user.avatar }} style={styles.avatarImage} />
-//               </View>
-//             </View>
-//           : // Show Please log in message if not
-//             <View style={styles.content}>
-//               <Text style={styles.header}>
-//                 Welcome Stranger!
-//               </Text>
-//               <View style={styles.avatar}>
-//                 <Icon name="user-circle" size={100} color="rgba(0,0,0,.09)" />
-//               </View>
-//               <Text style={styles.text}>
-//                 Please log in to continue {'\n'}
-//                 to MapGruff
-//               </Text>
-//             </View>
-//           }
-//           {/* Login buttons */}
-//           <View style={styles.buttons}>
-//             <Icon.Button
-//               name="facebook"
-//               backgroundColor="#3b5998"
-//               onPress={this.loginWithFacebook}
-//               {...iconStyles}
-//             >
-//               Login with Facebook
-//             </Icon.Button>
-//             <Icon.Button
-//               name="google"
-//               backgroundColor="#DD4B39"
-//               onPress={this.loginWithGoogle}
-//               {...iconStyles}
-//             >
-//               Login with Google
-//             </Icon.Button>
-//           </View>
-//         </View>
-//     );
-//   }
-// }
-
-// const iconStyles = {
-//   borderRadius: 10,
-//   iconStyle: { paddingVertical: 5 },
-// };
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#FFF',
-//   },
-//   content: {
-//     flex: 1,
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//   },
-//   avatar: {
-//     margin: 20,
-//   },
-//   avatarImage: {
-//     borderRadius: 50,
-//     height: 100,
-//     width: 100,
-//   },
-//   header: {
-//     fontSize: 20,
-//     textAlign: 'center',
-//     margin: 10,
-//   },
-//   text: {
-//     textAlign: 'center',
-//     color: '#333',
-//     marginBottom: 5,
-//   },
-//   buttons: {
-//     justifyContent: 'space-between',
-//     flexDirection: 'row',
-//     margin: 20,
-//     marginBottom: 30,
-//   },
-// });
-
-// export default Landing;
-
-=======
 export default Landing;
->>>>>>> Ready for rebase
