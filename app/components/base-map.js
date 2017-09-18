e72d709fe95bb09fc1d5449940df0006e94a40d6
Brian Kim <briankim4311@gmail.com>
2017-09-18 16:11:48 -0700

    Clean up base-map.js

--- app/components/base-map.js
+++ app/components/base-map.js
@@ -298,46 +298,6 @@ export default class BaseMap extends Component {
   	});
   }
 
-  addNewMarkers = () => {
-    // Treat annotations as immutable and create a new one instead of using .push()
-    this.setState({
-      annotations: [ ...this.state.annotations, {
-        coordinates: [40.73312,-73.989],
-        type: 'point',
-        title: 'This is a new marker',
-        id: 'foo'
-      }, {
-        'coordinates': [[40.749857912194386, -73.96820068359375], [40.741924698522055,-73.9735221862793], [40.735681504432264,-73.97523880004883], [40.7315190495212,-73.97438049316406], [40.729177554196376,-73.97180557250975], [40.72345355209305,-73.97438049316406], [40.719290332250544,-73.97455215454102], [40.71369559554873,-73.97729873657227], [40.71200407096382,-73.97850036621094], [40.71031250340588,-73.98691177368163], [40.71031250340588,-73.99154663085938]],
-        'type': 'polygon',
-        'fillAlpha': 1,
-        'fillColor': '#000000',
-        'strokeAlpha': 1,
-        'id': 'new-black-polygon'
-      }]
-    });
-  };
-
-  updateMarker2 = () => {
-    // Treat annotations as immutable and use .map() instead of changing the array
-    this.setState({
-      annotations: this.state.annotations.map(annotation => {
-        if (annotation.id !== 'marker2') { return annotation; }
-        return {
-          coordinates: [40.714541341726175,-74.00579452514648],
-          'type': 'point',
-          title: 'New Title!',
-          subtitle: 'New Subtitle',
-          annotationImage: {
-            source: { uri: 'https://cldup.com/7NLZklp8zS.png' },
-            height: 25,
-            width: 25
-          },
-          id: 'marker2'
-        };
-      })
-    });
-  };
-
   removeMarker2 = () => {
     this.setState({
       annotations: this.state.annotations.filter(a => a.id !== 'marker2')
@@ -400,7 +360,7 @@ export default class BaseMap extends Component {
             <TouchableHighlight style={mapStyle.currentLocation}>
               <Text onPress={ () => this.setState({ userTrackingMode: Mapbox.userTrackingMode.followWithHeading })} >{ locationIcon }</Text>
             </TouchableHighlight>
-            <TouchableHighlight style={mapStyle.crimeView} >
+            <TouchableHighlight style={mapStyle.crimeView}>
               <View>
                 {this.state.hideCrimes &&
                   <Text onPress={ () => this.onCrimesToggleClick()} >{ noViewIcon }</Text>
