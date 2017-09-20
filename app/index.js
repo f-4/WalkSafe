501b6bbc57863d81846f55f40c49c08bdb8b1f39
Rick Gallegos <duulketariakan@yahoo.com>
2017-09-19 21:37:57 -0700

    Ready for rebase

--- app/index.js
+++ app/index.js
@@ -2,7 +2,7 @@ console.log('index.js line 1')
 import React, { Component } from 'react';
 import { Root } from './config/routes';
 
-console.log('index.js line 3')
+console.log('index.js line 3');
 
 class App extends Component {
   render() {
