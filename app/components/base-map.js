7098eeebb74115fc9af0c55122de16258c009b5c
Brian Kim <briankim4311@gmail.com>
2017-09-20 14:55:40 -0700

    Convert coordinates to address for marked unsafe marker

--- app/components/base-map.js
+++ app/components/base-map.js
@@ -200,28 +200,39 @@ export default class BaseMap extends Component {
   };
 
   onLongPress = (location) => {
-    // Add Marked Unsafe marker on long press
-    this.setState({
-      annotations: [...this.state.annotations, {
-        coordinates: [location.latitude, location.longitude],
-        type: 'point',
-        title: 'Marked Unsafe',
-        subtitle: new Date().toLocaleString('en-US'),
-        annotationImage: {
-          source: { uri: 'http://www.freeiconspng.com/uploads/emergency-alert-icon-alert-icon-8.png' },
-          height: 25,
-          width: 25
-        },
-        rightCalloutAccessory: {
-          source: {
-            uri: 'https://openclipart.org/image/2400px/svg_to_png/16155/milker-X-icon.png'
-          },
-          height: 15,
-          width: 15,
-        },
-        id: `${location.latitude}, ${location.longitude}`
-      }]
-    });
+    // Retrieve address of long pressed location
+    axios.get(`${HOST}:${PORT}/api/map/geocode/reverse`, {
+      params: {
+        latitude: location.latitude,
+        longitude: location.longitude
+      }
+    })
+      .then(res => {
+        // Save address to use for annotation subtitle
+        const address = res.data.place_name.split(',');
+        // Add Marked Unsafe marker to state
+        this.setState({
+          annotations: [...this.state.annotations, {
+            coordinates: [location.latitude, location.longitude],
+            type: 'point',
+            title: 'Marked Unsafe',
+            subtitle: address.slice(0, 3).toString(),
+            annotationImage: {
+              source: { uri: 'http://www.freeiconspng.com/uploads/emergency-alert-icon-alert-icon-8.png' },
+              height: 25,
+              width: 25
+            },
+            rightCalloutAccessory: {
+              source: {
+                uri: 'https://openclipart.org/image/2400px/svg_to_png/16155/milker-X-icon.png'
+              },
+              height: 15,
+              width: 15,
+            },
+            id: `${location.latitude}, ${location.longitude}`
+          }]
+        });
+      });
   };
 
   onChangeUserTrackingMode = (userTrackingMode) => {
