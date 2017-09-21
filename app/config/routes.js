98be035bea0918d286778b6c987c18dab7c88d83
fredesse <eleegia@gmail.com>
2017-09-21 16:29:38 -0700

    Stub out tutorial screens

--- app/config/routes.js
+++ app/config/routes.js
@@ -1,82 +1,3 @@
-// import { DrawerNavigator, StackNavigator } from 'react-navigation';
-//
-// import Landing from '../screens/Landing';
-// import MyHomeScreen from '../screens/Home';
-// import CallUber from '../screens/CallUber';
-// import Contacts from '../screens/Contacts';
-// import About from '../screens/About';
-// import Tutorial from '../screens/Tutorial';
-// import MyNineOneOne from '../screens/NineOneOne';
-//
-// import Profile from '../components/icons/Profile';
-// import OrderUber from '../components/icons/Uber';
-// import Call911 from '../components/icons/Call911';
-// import EmergencyContacts from '../components/icons/EmergencyContacts';
-// import AboutIcon from '../components/icons/About';
-// import LogoutIcon from '../components/icons/Logout';
-// import TutorialIcon from '../components/icons/Tutorial';
-//
-// import Logout from '../components/buttons/Logout';
-//
-// export const HomeScreen = DrawerNavigator({
-//   Home: {
-//     screen: MyHomeScreen,
-//     navigationOptions: {
-//       drawerLabel: Profile,
-//     },
-//   },
-//   Uber: {
-//     screen: CallUber,
-//     navigationOptions: {
-//       drawerLabel: OrderUber
-//     },
-//   },
-//   NineOneOne: {
-//     screen: MyNineOneOne,
-//     navigationOptions: {
-//       drawerLabel: Call911,
-//     },
-//   },
-//   Contacts: {
-//     screen: Contacts,
-//     navigationOptions: {
-//       drawerLabel: EmergencyContacts,
-//     },
-//   },
-//   About: {
-//     screen: About,
-//     navigationOptions: {
-//       drawerLabel: AboutIcon,
-//     },
-//   },
-//   Tutorial: {
-//     screen: Tutorial,
-//     navigationOptions: {
-//       drawerLabel: TutorialIcon,
-//     },
-//   },
-//   Logout: {
-//     screen: Logout,
-//     navigationOptions: {
-//       drawerLabel: LogoutIcon,
-//     },
-//   },
-// });
-//
-// export const Root = StackNavigator({
-//   Landing: {
-//     screen: Landing,
-//   },
-//   Home: {
-//     screen: HomeScreen,
-//     navigationOptions: {
-//       gesturesEnabled: false,
-//     },
-//   },
-// }, {
-//   headerMode: 'none',
-// });
-
 import { DrawerNavigator, StackNavigator } from 'react-navigation';
 
 import Landing from '../screens/Landing';
@@ -86,34 +7,13 @@ import Contacts from '../screens/Contacts';
 import About from '../screens/About';
 import Tutorial from '../screens/Tutorial';
 import MyNineOneOne from '../screens/NineOneOne';
+import DrawerMenu from '../screens/Menu';
 
 import TutorialAbout from '../screens/tutorial/About';
 import Heatmap from '../screens/tutorial/Heatmap';
 import Legend from '../screens/tutorial/Legend';
 import CrimeIcons from '../screens/tutorial/CrimeIcons';
-
-import Profile from '../components/icons/Profile';
-import OrderUber from '../components/icons/Uber';
-import Call911 from '../components/icons/Call911';
-import EmergencyContacts from '../components/icons/EmergencyContacts';
-import AboutIcon from '../components/icons/About';
-import LogoutIcon from '../components/icons/Logout';
-import TutorialIcon from '../components/icons/Tutorial';
-
-import Logout from '../components/buttons/Logout';
-
-import DrawerMenu from '../screens/Menu';
-
-// const contactNavigator = StackNavigator({
-//   Contacts: {
-//     screen: Contacts,
-//     navigationOptions: ({navigation}) => ({
-//       headerStyle: {backgroundColor: 'red'},
-//       title: 'Contacts',
-//       headerTintColor: 'white'
-//     })
-//   },
-// });
+import Techstack from '../screens/tutorial/Techstack';
 
 const tutorialNavigator = StackNavigator({
   About: {
@@ -127,7 +27,10 @@ const tutorialNavigator = StackNavigator({
   },
   CrimeIcons: {
     screen: CrimeIcons
-  }
+  },
+  Techstack: {
+    screen: Techstack
+  },
 }, {
   headerMode: 'none'
 });
@@ -151,14 +54,9 @@ const stackNavigator = DrawerNavigator({
       gesturesEnabled: false,
     },
   },
-  Logout: {
-    screen: Logout
-  },
 }, {
   contentComponent: DrawerMenu,
 });
-
-
 
 export const Root = StackNavigator({
   Landing: {
@@ -170,24 +68,3 @@ export const Root = StackNavigator({
 }, {
   headerMode: 'none'
 });
-
-// export const Root = DrawerNavigator({
-//   Landing: {
-//     screen: Landing,
-//     navigationOptions: {
-//       drawerLockMode: 'locked-closed'
-//     }
-//   },
-//   Home: {
-//     screen: MyHomeScreen,
-//     navigationOptions: {
-//       gesturesEnabled: false,
-//       header: null
-//     },
-//   },
-//   Stack: {
-//     screen: stackNavigator
-//   }
-// }, {
-//   contentComponent: DrawerMenu,
-// });
